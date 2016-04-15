1a8a94a6afe3a03ee871feea3aa52b50880ca56a
Naveen Kumar <naveen3992@gmail.com>
2016-04-15 17:10:03 +0530

    First Commit Zomato

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zomato::Application.config.secret_token = 'a162dfe9cc95bd9efb5c77dd70777d4da700bde71f8d76d27d174b42c994d20cd2fdf917f1c6da20174456abbbd64ed3d09a991f616df2c518f6d274852b07f2'
