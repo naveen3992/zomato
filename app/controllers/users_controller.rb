7646f7873082d99fb779d656fd974956a1f43611
Naveen Kumar <naveen3992@gmail.com>
2016-04-18 15:26:11 +0530

    sign up complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,22 @@ class UsersController < ApplicationController
   def create 
   	@user =User.new(params[:user])
   	if @user.save 
+  		flash[:success] = "Welcome to Zomato!"
+  		redirect_to @user
   	else
   	render 'new'
     end
   end
+
+  def new 
+  	@user=User.new
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 end
